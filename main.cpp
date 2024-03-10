63acd0acca8ecfad23a0285b36a114674c1d5e2f
NikolfaxW <Denisnik@cvut.cz>
2024-03-10 23:47:23 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "Pythia8/Pythia.h"
+#include "TCanvas.h"
+#include "TString.h"
+#include "TH2D.h"
+#include "TMath.h"
+#include "TPave.h"
+#include "TMarker.h"
+#include "TLatex.h"
+#include "TRandom3.h"
+#include "TStyle.h"
+#include "fastjet/PseudoJet.hh"
+#include "fastjet/ClusterSequence.hh"
+
+#include "drawF.h"
+
+int main() {
+
+
+    return 0;
+}
