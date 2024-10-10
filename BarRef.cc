613c77724ffbb6fe4f952ac075b5f445958ece3d
NikolfaxW <Denisnik@cvut.cz>
2024-10-10 22:08:42 +0200

    Ready version of the program. Now it finds events with d_0. However, it does not idetify which jets have it

--- BarRef.cc
+++ BarRef.cc
@@ -4,8 +4,8 @@
 
 #include <iostream>
 #include <fstream>
-#include <sstream>  
-#include <cstdio>   
+#include <sstream>
+#include <cstdio>
 #include "Pythia8/Pythia.h"
 #include "Pythia8Plugins/FastJet3.h"
 
@@ -32,16 +32,16 @@ using namespace Pythia8;
 class MyInfo: public PseudoJet::UserInfoBase {
 
 public:
-MyInfo(const int & id, const int & i, const int & ch) : _pdg_id(id), _id(i), _ch(ch) {}
+    MyInfo(const int & id, const int & i, const int & ch) : _pdg_id(id), _id(i), _ch(ch) {}
 
-int pdg_id() 	const {return _pdg_id;}
-int id()		const {return _id;}
-int ch()		const {return _ch;}
+    int pdg_id() 	const {return _pdg_id;}
+    int id()		const {return _id;}
+    int ch()		const {return _ch;}
 
 protected:
-int _pdg_id;
-int _id;
-int _ch;
+    int _pdg_id;
+    int _id;
+    int _ch;
 
 };
 
@@ -53,217 +53,217 @@ double delta_R(double eta1, double phi1, double eta2, double phi2) {
 
 int main(int argc, char* argv[]) {
 
-  
-  // Select common parameters for FastJet analysis.
-  double Rparam = 0.4;
-  fastjet::JetAlgorithm 		algorithm = fastjet::antikt_algorithm;
-  fastjet::RecombinationScheme	recombScheme = fastjet::E_scheme;
-  fastjet::Strategy 			strategy = fastjet::Best;
-  fastjet::JetDefinition		*jetDef = nullptr;
-  
-  int triggerId = 421; 
 
-  double pTMin = 0.2;			// Min particle pT
-  double pTMinTrig = 0.0;		// Min triger particle pT
-  double pTMinJet   = 0.0;      // Min jet pT.
-  double etaMax  = 1.0;    		// Pseudorapidity range of detector.
-  bool 	 chJets = 0;
-  bool 	 chConst = 1;
-  TString confFileName = TString(argv[1]).Remove(TString(argv[1]).Last('.'));
+    // Select common parameters for FastJet analysis.
+    double Rparam = 0.4;
+    fastjet::JetAlgorithm 		algorithm = fastjet::antikt_algorithm;
+    fastjet::RecombinationScheme	recombScheme = fastjet::E_scheme;
+    fastjet::Strategy 			strategy = fastjet::Best;
+    fastjet::JetDefinition		*jetDef = nullptr;
 
-  // Create a ROOT file to store the output  
-  TString outputFileName;
-  if(chJets) outputFileName = TString::Format("out/pythia8_chargedJets_%s_R0%.f_hfpt%.1f_%s.root",confFileName.Data(), Rparam*10, pTMinTrig, argv[2]);
-  else  if(chConst) outputFileName = TString::Format("out/pythia8_inclusiveJets_chargedConst_%s_R0%.f_hfpt%.1f_%s.root",confFileName.Data(), Rparam*10, pTMinTrig, argv[2]);
-  else outputFileName = TString::Format("out/pythia8_inclusiveJets_%s_R0%.f_hfpt%.1f_%s.root",confFileName.Data(), Rparam*10, pTMinTrig, argv[2]);
-  TFile* file = TFile::Open(outputFileName.Data(), "RECREATE");
-  // Create a TNtuple to store the event information
-  TNtuple ntuple("HFjets", "HF-jets Information", "pt_jet:eta_jet:pt_hf:eta_hf:y_hf:zvalue:lambda_1:lambda_15:lambda_2:lambda_3:lambda_4");
- // TNtuple *ntuple = new TNtuple("HFjets", "HF-jets Information", "pt_jet:eta_jet:pt_hf:eta_hf:zvalue:lambda_1:lambda_15:lambda_2:lambda_3:lambda_4");
-   Long64_t maxFileSize = 1e7;  // 0.01 GB (adjust this value as needed)
- // ntuple->SetMaxTreeSize(maxFileSize);
-   ntuple.SetMaxTreeSize(maxFileSize);
-    
-  // Generator. Shorthand for event.
-  Pythia pythia;
-  Event& event = pythia.event;
-  
-  // Extract settings to be used in the main program.
-  pythia.readFile(argv[1]);
-  int nEvent = pythia.mode("Main:numberOfEvents");
-  int nAbort = pythia.mode("Main:timesAllowErrors");
-    
-  pythia.init();
+    int triggerId = 421;
 
-  // Set up FastJet jet finder.
-  jetDef = new fastjet::JetDefinition(algorithm, Rparam,
-                                      recombScheme, strategy);
-  // Fastjet input
-  std::vector <fastjet::PseudoJet> fjInputs;
+    double pTMin = 0.2;			// Min particle pT
+    double pTMinTrig = 0.0;		// Min triger particle pT
+    double pTMinJet   = 0.0;      // Min jet pT.
+    double etaMax  = 1.0;    		// Pseudorapidity range of detector.
+    bool 	 chJets = 0;
+    bool 	 chConst = 1;
+    TString confFileName = TString(argv[1]).Remove(TString(argv[1]).Last('.'));
 
-  // Begin event loop. Generate event. Skip if error.
-  for (int iEvent = 0; iEvent < nEvent; ++iEvent) {
-    
-    if (!pythia.next()) continue;
-   
-    // Find trigger particle 
-    int idxD = -1;
-    double pt, eta;
-    for (int i = pythia.event.size() - 1; i > 0; i--) {
-      if (pythia.event[i].idAbs() == triggerId && pythia.event[i].pT() >= pTMinTrig) {
-        idxD = i;
-        pt = pythia.event[i].pT();
-        eta = pythia.event[i].eta();
-        break;
-      }
+    // Create a ROOT file to store the output
+    TString outputFileName;
+    if(chJets) outputFileName = TString::Format("out/pythia8_chargedJets_%s_R0%.f_hfpt%.1f_%s.root",confFileName.Data(), Rparam*10, pTMinTrig, argv[2]);
+    else  if(chConst) outputFileName = TString::Format("out/pythia8_inclusiveJets_chargedConst_%s_R0%.f_hfpt%.1f_%s.root",confFileName.Data(), Rparam*10, pTMinTrig, argv[2]);
+    else outputFileName = TString::Format("out/pythia8_inclusiveJets_%s_R0%.f_hfpt%.1f_%s.root",confFileName.Data(), Rparam*10, pTMinTrig, argv[2]);
+    TFile* file = TFile::Open(outputFileName.Data(), "RECREATE");
+    // Create a TNtuple to store the event information
+    TNtuple ntuple("HFjets", "HF-jets Information", "pt_jet:eta_jet:pt_hf:eta_hf:y_hf:zvalue:lambda_1:lambda_15:lambda_2:lambda_3:lambda_4");
+    // TNtuple *ntuple = new TNtuple("HFjets", "HF-jets Information", "pt_jet:eta_jet:pt_hf:eta_hf:zvalue:lambda_1:lambda_15:lambda_2:lambda_3:lambda_4");
+    Long64_t maxFileSize = 1e7;  // 0.01 GB (adjust this value as needed)
+    // ntuple->SetMaxTreeSize(maxFileSize);
+    ntuple.SetMaxTreeSize(maxFileSize);
+
+    // Generator. Shorthand for event.
+    Pythia pythia;
+    Event& event = pythia.event;
+
+    // Extract settings to be used in the main program.
+    pythia.readFile(argv[1]);
+    int nEvent = pythia.mode("Main:numberOfEvents");
+    int nAbort = pythia.mode("Main:timesAllowErrors");
+
+    pythia.init();
+
+    // Set up FastJet jet finder.
+    jetDef = new fastjet::JetDefinition(algorithm, Rparam,
+                                        recombScheme, strategy);
+    // Fastjet input
+    std::vector <fastjet::PseudoJet> fjInputs;
+
+    // Begin event loop. Generate event. Skip if error.
+    for (int iEvent = 0; iEvent < nEvent; ++iEvent) {
+
+        if (!pythia.next()) continue;
+
+        // Find trigger particle
+        int idxD = -1;
+        double pt, eta;
+        for (int i = pythia.event.size() - 1; i > 0; i--) {
+            if (pythia.event[i].idAbs() == triggerId && pythia.event[i].pT() >= pTMinTrig) {
+                idxD = i;
+                pt = pythia.event[i].pT();
+                eta = pythia.event[i].eta();
+                break;
+            }
+        }
+        if (idxD == -1) {
+            //cout << "Error: Could not find trigger particle " << triggerId << endl;
+            continue;
+        }
+        //else cout << "Found trigger particle " << triggerId << "\t id = " << idxD << "\t in event: " << iEvent << "\t pt = " << pt << "\t eta = " << eta << endl;
+
+        fjInputs.resize(0);
+        Vec4   pTemp;
+        double mTemp;
+        int pdg_id, pdg_id_abs, charge;
+
+        for (int i = 0; i < pythia.event.size(); ++i) {
+            // Final state only
+            if (!pythia.event[i].isFinal())        continue;
+            if (chJets) { if(!pythia.event[i].isCharged() && pythia.event[i].idAbs() != triggerId)	continue; }
+
+            pdg_id = pythia.event[i].id();
+            pdg_id_abs = pythia.event[i].idAbs();
+            charge = pythia.event[i].isCharged();
+            // No neutrinos
+            //===================!!!! CHECK FOR SECONDARIES !!!!!!
+            if (pdg_id_abs == 12 || pdg_id_abs == 14 || pdg_id_abs == 16) continue;
+            // if (geantid == 4 || geantid == 5 || geantid == 6 || geantid == 10 || geantid == 16 || geantid == 17 || geantid == 18 || geantid == 20 || geantid == 22 || geantid == 26 || geantid == 28 || geantid == 30 ) continue;
+
+
+            // |eta| and pt acceptance
+            // ==================!!! SHOULD BE PARTICLE PT CUT BE USED ?
+            if (abs(pythia.event[i].eta()) > etaMax || pythia.event[i].pT() < pTMin) continue;
+
+            // Create a PseudoJet from the complete Pythia particle.
+            fastjet::PseudoJet particleTemp = pythia.event[i];
+
+            // Modify mass to pi0 or gamma mass
+            pTemp = pythia.event[i].p();
+            mTemp = pythia.event[i].m();
+            // ==================!!! CHECK THIS MASS SETTING, for D meson and other neutrals
+            mTemp = pdg_id_abs == 22 ? 0. : 0.13957;
+            pTemp.e( sqrt(pTemp.pAbs2() + mTemp*mTemp) );
+            particleTemp.reset_momentum( pTemp.px(), pTemp.py(), pTemp.pz(), pTemp.e() );
+            particleTemp.set_user_info(new MyInfo(pdg_id, i, charge)); //Needs to be implemented
+
+            // Store acceptable particles as input to Fastjet.
+            fjInputs.push_back(particleTemp);
+
+        }
+
+        if (fjInputs.size() == 0) {
+            cout << "Error: event with no final state particles" << endl;
+            continue;
+        }
+
+        // Run Fastjet algorithm and sort jets in pT order.
+        vector <fastjet::PseudoJet> inclusiveJets, sortedJets, selectedJets;
+        fastjet::ClusterSequence clustSeq(fjInputs, *jetDef);
+        inclusiveJets = clustSeq.inclusive_jets(pTMinJet);
+        sortedJets    = sorted_by_pt(inclusiveJets);
+
+        fastjet::Selector eta_selector = fastjet::SelectorEtaRange(-1+Rparam, 1-Rparam); // Selects jets with |eta| < 1-R
+        selectedJets = eta_selector(sortedJets);
+
+        //cout << "Number of jets: " << sortedJets.size() << "\t selected jets: " << selectedJets.size() << endl;
+
+        for(int jetid = 0; jetid < selectedJets.size(); jetid++){
+
+            vector <fastjet::PseudoJet> constituents = selectedJets[jetid].constituents();
+            vector <fastjet::PseudoJet> sortedconstituents = sorted_by_pt(constituents);
+
+            bool d0Jet = false;
+            double HFPx = -99.;
+            double HFPy = -99.;
+            double HFPt = -99.;
+            double HFEta = -99.;
+            double HFy = -99.;
+
+            double zvalue = -99.;
+            double lambda[5] = {0.,0.,0.,0.,0.};
+            double lambda_alpha[5] = {1.,1.5,2.,3.,4.};
+            double lambda_kappa = 1;
+
+            double JetPx = selectedJets[jetid].px();
+            double JetPy = selectedJets[jetid].py();
+            double JetPt = selectedJets[jetid].perp();
+            double JetEta = selectedJets[jetid].eta();
+            double JetPhi = selectedJets[jetid].phi();
+
+            // Loop over jet constituents to get some info
+            for (unsigned j = 0; j < sortedconstituents.size(); j++){
+                //cout << "id: " << abs(sortedconstituents[j].user_info<MyInfo>().pdg_id()) << endl;
+
+                double consPx = sortedconstituents[j].px();
+                double consPy = sortedconstituents[j].py();
+                double consPt = sortedconstituents[j].perp();
+                double consEta = sortedconstituents[j].eta();
+                double consPhi = sortedconstituents[j].phi();
+
+                double Delta_R = delta_R(JetEta,JetPhi,consEta,consPhi);
+
+                if (chConst){
+                    if(sortedconstituents[j].user_info<MyInfo>().ch() != 0) {
+                        for(int a = 0; a<5; a++) lambda[a]+=pow(consPt / JetPt, lambda_kappa)*pow( Delta_R / Rparam, lambda_alpha[a]);
+                    }
+                }
+                else {
+                    for(int a = 0; a<5; a++) lambda[a]+=pow(consPt / JetPt, lambda_kappa)*pow( Delta_R / Rparam, lambda_alpha[a]);
+                }
+                // Check if trigger particle
+                if (abs(sortedconstituents[j].user_info<MyInfo>().pdg_id()) == triggerId){ //!here it gets stored
+
+                    HFPx = sortedconstituents[j].px();
+                    HFPy = sortedconstituents[j].py();
+                    HFPt = sortedconstituents[j].perp();
+                    HFEta = sortedconstituents[j].eta();
+                    HFy = sortedconstituents[j].rap();
+
+                    if (HFPt < pTMinTrig) continue;
+
+                    d0Jet = true;
+
+                    zvalue = (JetPx*HFPx + JetPy*HFPy)/(JetPt * JetPt);
+
+                    //LOG_INFO << Form("Jet found with pT: %f, D0 with pT: %f, z: %f", MCJetPt, MCD0Pt, zvalue) << endl;
+                }
+
+                // End of jet constituent loop
+            }
+
+            if (d0Jet) ntuple.Fill(JetPt, JetEta, HFPt, HFEta, HFy, zvalue, lambda[0],lambda[1], lambda[2], lambda[3], lambda[4]);
+            //if (d0Jet) ntuple -> Fill(JetPt, JetEta, HFPt, HFEta, zvalue, lambda[0],lambda[1], lambda[2], lambda[3], lambda[4]);
+
+
+            // End of jet loop
+        }
+
+        // End of event loop.
     }
-    if (idxD == -1) {
-      //cout << "Error: Could not find trigger particle " << triggerId << endl;
-      continue; //!skips the event withou D_0
-    }
-   //else cout << "Found trigger particle " << triggerId << "\t id = " << idxD << "\t in event: " << iEvent << "\t pt = " << pt << "\t eta = " << eta << endl;
-	
-    fjInputs.resize(0); //!?
-    Vec4   pTemp;
-    double mTemp;
-    int pdg_id, pdg_id_abs, charge;
- 
-    for (int i = 0; i < pythia.event.size(); ++i) {
-      // Final state only
-      if (!pythia.event[i].isFinal())        continue;
-      if (chJets) { if(!pythia.event[i].isCharged() && pythia.event[i].idAbs() != triggerId)	continue; } //! what is chJets
-     
-      pdg_id = pythia.event[i].id();
-      pdg_id_abs = pythia.event[i].idAbs();
-      charge = pythia.event[i].isCharged();
-      // No neutrinos
-      //===================!!!! CHECK FOR SECONDARIES !!!!!!
-      if (pdg_id_abs == 12 || pdg_id_abs == 14 || pdg_id_abs == 16) continue;
-      // if (geantid == 4 || geantid == 5 || geantid == 6 || geantid == 10 || geantid == 16 || geantid == 17 || geantid == 18 || geantid == 20 || geantid == 22 || geantid == 26 || geantid == 28 || geantid == 30 ) continue;
-      
 
-      // |eta| and pt acceptance
-      // ==================!!! SHOULD BE PARTICLE PT CUT BE USED ?
-      if (abs(pythia.event[i].eta()) > etaMax || pythia.event[i].pT() < pTMin) continue;
+    //ntuple->Write();
+    ntuple.Write();
+    file->Close();
 
-      // Create a PseudoJet from the complete Pythia particle.
-      fastjet::PseudoJet particleTemp = pythia.event[i];
-      // Modify mass to pi0 or gamma mass
-      pTemp = pythia.event[i].p();
-      mTemp = pythia.event[i].m(); 
-      // ==================!!! CHECK THIS MASS SETTING, for D meson and other neutrals
-      mTemp = pdg_id_abs == 22 ? 0. : 0.13957; //! ?
-      pTemp.e( sqrt(pTemp.pAbs2() + mTemp*mTemp) );
-      particleTemp.reset_momentum( pTemp.px(), pTemp.py(), pTemp.pz(), pTemp.e() );
-      particleTemp.set_user_info(new MyInfo(pdg_id, i, charge));
-      
-      // Store acceptable particles as input to Fastjet.
-      fjInputs.push_back(particleTemp);
-      
-    }
+    // Statistics. Histograms.
+    pythia.stat();
 
-    if (fjInputs.size() == 0) {
-      cout << "Error: event with no final state particles" << endl;
-      continue;
-    }
+    //cout <<  nJetsF << pTjetsF << RdistF << distJets << pTdiff << nAna << tGen;
 
-    // Run Fastjet algorithm and sort jets in pT order.
-    vector <fastjet::PseudoJet> inclusiveJets, sortedJets, selectedJets;
-    fastjet::ClusterSequence clustSeq(fjInputs, *jetDef);
-    inclusiveJets = clustSeq.inclusive_jets(pTMinJet);
-    sortedJets    = sorted_by_pt(inclusiveJets);
-
-    fastjet::Selector eta_selector = fastjet::SelectorEtaRange(-1+Rparam, 1-Rparam); // Selects jets with |eta| < 1-R
-    selectedJets = eta_selector(sortedJets);
-    
-    //cout << "Number of jets: " << sortedJets.size() << "\t selected jets: " << selectedJets.size() << endl;
-
-    for(int jetid = 0; jetid < selectedJets.size(); jetid++){
-    	
-    	vector <fastjet::PseudoJet> constituents = selectedJets[jetid].constituents();
-    	vector <fastjet::PseudoJet> sortedconstituents = sorted_by_pt(constituents);
-    
-    	bool d0Jet = false;
-		double HFPx = -99.;
-        double HFPy = -99.;
-        double HFPt = -99.;
-        double HFEta = -99.;
-        double HFy = -99.;
-        	
-    	double zvalue = -99.;
-    	double lambda[5] = {0.,0.,0.,0.,0.};
-    	double lambda_alpha[5] = {1.,1.5,2.,3.,4.};
-		double lambda_kappa = 1;
-    
-		double JetPx = selectedJets[jetid].px();
-        double JetPy = selectedJets[jetid].py();
-        double JetPt = selectedJets[jetid].perp();
-        double JetEta = selectedJets[jetid].eta(); 
-        double JetPhi = selectedJets[jetid].phi(); 
-
-		// Loop over jet constituents to get some info
-    	for (unsigned j = 0; j < sortedconstituents.size(); j++){
-			//cout << "id: " << abs(sortedconstituents[j].user_info<MyInfo>().pdg_id()) << endl;
-			
-			 double consPx = sortedconstituents[j].px();
-        	 double consPy = sortedconstituents[j].py();
-        	 double consPt = sortedconstituents[j].perp();
-        	 double consEta = sortedconstituents[j].eta();
-        	 double consPhi = sortedconstituents[j].phi();
-			 
-			 double Delta_R = delta_R(JetEta,JetPhi,consEta,consPhi);
-             
-             if (chConst){
-				if(sortedconstituents[j].user_info<MyInfo>().ch() != 0) { //!what os lmbda
-					for(int a = 0; a<5; a++) lambda[a]+=pow(consPt / JetPt, lambda_kappa)*pow( Delta_R / Rparam, lambda_alpha[a]);
-				}
-			}	
-			else {
-				for(int a = 0; a<5; a++) lambda[a]+=pow(consPt / JetPt, lambda_kappa)*pow( Delta_R / Rparam, lambda_alpha[a]);
-			}
-			// Check if trigger particle
-      		if (abs(sortedconstituents[j].user_info<MyInfo>().pdg_id()) == triggerId) //!only here?
-			
-			HFPx = sortedconstituents[j].px();
-        	HFPy = sortedconstituents[j].py();
-        	HFPt = sortedconstituents[j].perp();
-        	HFEta = sortedconstituents[j].eta();
-        	HFy = sortedconstituents[j].rap();
-        	
-			if (HFPt < pTMinTrig) continue;
-			
-			d0Jet = true;
-		
-        	zvalue = (JetPx*HFPx + JetPy*HFPy)/(JetPt * JetPt);
-       	     
-        	//LOG_INFO << Form("Jet found with pT: %f, D0 with pT: %f, z: %f", MCJetPt, MCD0Pt, zvalue) << endl;
-      		}
-      		
-    	// End of jet constituent loop
-    	} 
-    	
-    	if (d0Jet) ntuple.Fill(JetPt, JetEta, HFPt, HFEta, HFy, zvalue, lambda[0],lambda[1], lambda[2], lambda[3], lambda[4]);
-    	//if (d0Jet) ntuple -> Fill(JetPt, JetEta, HFPt, HFEta, zvalue, lambda[0],lambda[1], lambda[2], lambda[3], lambda[4]);
-
-    	
-	// End of jet loop
-	} 
-
-  // End of event loop.
-  }
-
-  //ntuple->Write();
-  ntuple.Write();
-  file->Close();
-
-  // Statistics. Histograms.
-  pythia.stat();
-  
-  //cout <<  nJetsF << pTjetsF << RdistF << distJets << pTdiff << nAna << tGen;
-
-  // Done.
-  return 0;
+    // Done.
+    return 0;
 
 }
-
 
--- main.cpp
+++ main.cpp
@@ -16,37 +16,37 @@
 #include "particleUnit.h"
 
 
-
-class MyInfo: public fastjet::UserInfoBase {
+//simple class to store additional info for fastjet
+class MyInfo: public fastjet::PseudoJet::UserInfoBase { //here also was charge attribute
 
 public:
-    MyInfo(const int & id, const int & i, const int & ch) : _pdg_id(id), _id(i), _ch(ch) {}
+    MyInfo(const int & id, const int & i) : _pdg_id(id), _id(i){}
 
     int pdg_id() 	const {return _pdg_id;}
     int id()		const {return _id;}
-    int ch()		const {return _ch;}
 
 protected:
     int _pdg_id;
     int _id;
-    int _ch;
-};
 
+};
 
 int main() {
     Pythia8::Pythia pythia;
     pythia.readFile("../config1.cmnd");
     pythia.init();
 
-    TFile *file = new TFile("../results/Jet_tree.root", "RECREATE");
-    TTree *T = new TTree("T", "saves Pt, pseudo rapidity and phi of an jet");
-    Float_t pT, gamma, phi; /*j_iter = 0*/
+    TFile *file = new TFile("../results/Jet_tree2.root", "RECREATE");
+    TTree *T = new TTree("T", "saves Pt, pseudo rapidity and phi of an D_0 jet");
+    Float_t pT, gamma, phi, /*j_iter = 0*/ hasD_0, EVI;
     std::list<particleUnit> j_constituents;
 //    T->Branch("Jet id", &j_iter, "Jet id");//!
+    T->Branch("EventID", &EVI, "EventID");
     T->Branch("pT", &pT, "pT");
     T->Branch("eta", &gamma, "eta");
     T->Branch("phi", &phi, "phi");
-    T->Branch("jet constituents", &j_constituents, "jet constituents");
+    T->Branch("D_0", &hasD_0, "D_0");
+//    T->Branch("jet constituents", &j_constituents, "jet constituents");
 
 
     std::map<TString, fastjet::JetDefinition> jetDefs;
@@ -56,7 +56,9 @@ int main() {
     double pTmin_jet = 5, pThadron = 0.2;
     int triggerId = 421; //!D_0
     double pTMinTrig = 0.0;
-    double mTemp = 0;
+    double mTemp; //This variables are needed to recount momentum after particle mass resets
+    Pythia8::Vec4 pTemp;
+
 
     TString description = "Number of events: " + std::to_string(pythia.mode("Main:numberOfEvents"));
 
@@ -72,6 +74,7 @@ int main() {
 
     auto &event = pythia.event;
     std::vector<Pythia8::Particle> particles_histogram;
+    std::vector<fastjet::PseudoJet> fjInputs;
     std::vector<fastjet::PseudoJet> stable_particles;
     std::vector<fastjet::PseudoJet> selectedJets;
 
@@ -95,50 +98,79 @@ int main() {
         if(idxD == -1) //! works fine
             continue; //! D_0 was not found in the event
 //        else{
-//            std::cout << "D_0 was found in the event" << std::endl;
+//            std::cout << "D_0 was found in the event number : " << iEvent << std::endl;
 //        }
 
 
-
+        fjInputs.clear();
         for (int i = 0; i < event.size(); ++i) { // saves particles in order to make jets
-            auto &p = event[i];
-            if(not p.isFinal())
+            auto & p = event[i];
+            if(not event[i].isFinal())
                 continue;
-            if(p.id() == 22) //changes mass for any other particle except photons to pion mass
+            pTemp = p.p();
+            if(p.idAbs() == 22) //changes mass for any other particle except photons to pion mass
                 mTemp = 0;
             else
                 mTemp = 0.13957;
-            p.e(sqrt(p.pAbs2() + mTemp * mTemp);
-            int pdg_id_abs = std::abs(p.id());
-            mTemp = pdg_id_abs == 22 ? 0. : 0.13957;
-            stable_particles.push_back(fastjet::PseudoJet(p.px(), p.py(), p.pz(), p.e()));
-            particles_histogram.push_back(p);
+
+//            fastjet::PseudoJet particleTemp = pythia.event[i] //!Does not work
+            fastjet::PseudoJet particleTemp = fastjet::PseudoJet(p.px(), p.py(), p.pz(), sqrt(pTemp.pAbs2() + mTemp*mTemp));  //comps energy with new mass
+            //idk if it is needed to correct boost or no. Let's consider 'no'.
+            particleTemp.set_user_info(new MyInfo(p.idAbs(), i));
+            fjInputs.push_back(particleTemp);
+
+
+//            int pdg_id_abs = std::abs(p.id());
+//            stable_particles.push_back(fastjet::PseudoJet(p.px(), p.py(), p.pz(), p.e()));
+//            particles_histogram.push_back(p);
         }
 
 
+        if (fjInputs.empty()) { // Abort to avoid running algs through empty vecs
+            std::cout << "Error: event with no final state particles" << std::endl;
+            continue;
+        }
 
 
         for (auto jetDef: jetDefs) { //for each jet definition runs jet clustering sequance, then saves it to ROOT tree
-            fastjet::ClusterSequence clustSeq(stable_particles, jetDef.second);
-            auto jets = sorted_by_pt(clustSeq.inclusive_jets(pTmin_jet));
+            selectedJets.clear();
+            fastjet::ClusterSequence clustSeq(fjInputs, jetDef.second); //changed from  stable_particles
+            auto jets = sorted_by_pt(clustSeq.inclusive_jets(0)); //!! For some reason sorting requires much lower ptmin than pTmin_jet
             fastjet::Selector eta_selector = fastjet::SelectorEtaRange(-1 + R, 1 - R); // Selects jets with |eta| < 1-R
             selectedJets = eta_selector(jets);
 
 
-            for (auto jet: selectedJets) {
-                if (jet.pt() < pTmin_jet) continue; // skipping selectedJets with low rapidity
-                j_constituents.clear();
-                // For each particle:
-//                ++j_iter;
-                for (auto c: jet.constituents()) {
-                    if (c.pt() < pThadron) continue; //gets rid of bubbles in selectedJets
-                    j_constituents.push_back(particleUnit(c.pt(), c.rapidity(), c.phi()));
+//            for (auto jet: selectedJets) { // writes constituents of jets
+//                if (jet.pt() < pTmin_jet) continue; // skipping selectedJets with low rapidity
+//                j_constituents.clear();
+//                // For each particle:
+////                ++j_iter;
+//                for (auto c: jet.constituents()) {
+//                    if (c.pt() < pThadron) continue; //gets rid of bubbles in selectedJets
+//                    j_constituents.push_back(particleUnit(c.pt(), c.rapidity(), c.phi()));
+//                }
+//                pT = jet.pt();
+//                gamma = jet.rapidity();
+//                phi = jet.phi();
+//                T->Fill();
+//            }
+            for(const auto& jet: selectedJets){ //!EMPTY
+
+//                if (jet.pt() < pTmin_jet) continue; // skipping selectedJets with low rapidity //SAME PROBLEM HERE
+                hasD_0 = 0;
+                for(const auto& c : jet.constituents() ){
+                    if(c.user_info<MyInfo>().pdg_id() == triggerId){
+                        hasD_0 = 1;
+                        break;
+                    }
                 }
+                EVI = iEvent;
                 pT = jet.pt();
                 gamma = jet.rapidity();
                 phi = jet.phi();
                 T->Fill();
             }
+
         }
     } //move it to the end in order to split events
     T->Print();
