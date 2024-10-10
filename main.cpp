082cb260e7f46a42a261c8af91e10900c4a3b963
NikolfaxW <denisnik@cvut.cz>
2024-10-10 17:18:16 +0200

    first part of d_0 indetication

--- main.cpp
+++ main.cpp
@@ -16,6 +16,23 @@
 #include "particleUnit.h"
 
 
+
+class MyInfo: public fastjet::UserInfoBase {
+
+public:
+    MyInfo(const int & id, const int & i, const int & ch) : _pdg_id(id), _id(i), _ch(ch) {}
+
+    int pdg_id() 	const {return _pdg_id;}
+    int id()		const {return _id;}
+    int ch()		const {return _ch;}
+
+protected:
+    int _pdg_id;
+    int _id;
+    int _ch;
+};
+
+
 int main() {
     Pythia8::Pythia pythia;
     pythia.readFile("../config1.cmnd");
@@ -37,8 +54,9 @@ int main() {
 
     double R = 0.4;
     double pTmin_jet = 5, pThadron = 0.2;
-    int triggerId = 421; //D_0
+    int triggerId = 421; //!D_0
     double pTMinTrig = 0.0;
+    double mTemp = 0;
 
     TString description = "Number of events: " + std::to_string(pythia.mode("Main:numberOfEvents"));
 
@@ -63,7 +81,8 @@ int main() {
         stable_particles.clear();
         if (!pythia.next()) continue;
 
-        //coppied
+        //coppied from the example
+        // Find the index of the D0 meson in an event and (optonally) its pT and eta
         int idxD = -1;
         for (int i = pythia.event.size() - 1; i > 0; i--) {
             if (pythia.event[i].idAbs() == triggerId && pythia.event[i].pT() >= pTMinTrig) {
@@ -73,23 +92,35 @@ int main() {
                 break;
             }
         }
-        //
+        if(idxD == -1) //! works fine
+            continue; //! D_0 was not found in the event
+//        else{
+//            std::cout << "D_0 was found in the event" << std::endl;
+//        }
 
 
 
-        for (int i = 0; i < event.size(); ++i) {
+        for (int i = 0; i < event.size(); ++i) { // saves particles in order to make jets
             auto &p = event[i];
-            if (not p.isFinal()) continue;
+            if(not p.isFinal())
+                continue;
+            if(p.id() == 22) //changes mass for any other particle except photons to pion mass
+                mTemp = 0;
+            else
+                mTemp = 0.13957;
+            p.e(sqrt(p.pAbs2() + mTemp * mTemp);
+            int pdg_id_abs = std::abs(p.id());
+            mTemp = pdg_id_abs == 22 ? 0. : 0.13957;
             stable_particles.push_back(fastjet::PseudoJet(p.px(), p.py(), p.pz(), p.e()));
             particles_histogram.push_back(p);
         }
 
 
-        for (auto jetDef: jetDefs) {
+
+
+        for (auto jetDef: jetDefs) { //for each jet definition runs jet clustering sequance, then saves it to ROOT tree
             fastjet::ClusterSequence clustSeq(stable_particles, jetDef.second);
             auto jets = sorted_by_pt(clustSeq.inclusive_jets(pTmin_jet));
-
-
             fastjet::Selector eta_selector = fastjet::SelectorEtaRange(-1 + R, 1 - R); // Selects jets with |eta| < 1-R
             selectedJets = eta_selector(jets);
 
@@ -108,9 +139,7 @@ int main() {
                 phi = jet.phi();
                 T->Fill();
             }
-
         }
-
     } //move it to the end in order to split events
     T->Print();
     T->Write();
