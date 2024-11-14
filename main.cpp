f52bfa25e1e2ac22372b744d48fdddb46bf0ea01
NikolfaxW <Denisnik@cvut.cz>
2024-11-14 12:42:57 +0100

    Some simulations done

--- main.cpp
+++ main.cpp
@@ -273,53 +273,52 @@ void mainSec(const int numThreads, std::string  seed, TTree *&T, Float_t &D_0_pT
   
 
 int main() {
-    learnD_0JetsOrigin(100);
+//    learnD_0JetsOrigin(100);
 
-//    unsigned int requiredNumberOfD_0 = 10000;
-//    unsigned int foundNumberOfD_0 = 0; //to store number of D_0 particles found
-//    unsigned int numThreads = std::thread::hardware_concurrency();
-//    int seed = std::time(0) % (900000000 - numThreads);
-////    unsigned int numThreads = 2;
-//    std::thread **alocatedThreads = new std::thread *[numThreads];
-//    std::string pathToTheFile = "../results/";
-//    std::string filename = "jet tree, cut [D_0 n =" + std::to_string(requiredNumberOfD_0) + ", 1+ GeV].root";
-//    TTree *T = new TTree("T", "saves Pt, pseudo rapidity and phi of an D_0 jet"); //create a tree to store the data
-//    TFile *file = new TFile( (pathToTheFile + filename).c_str(), "RECREATE"); //create a file to store the tree as an output
-//    Float_t D_0_pT = -999, Jet_pT = -999, rapidity = -999, z_val = -999; //variables to store data and used in tree
-//    Float_t l11, l105, l115, l12, l13, l20;
-//    bool hasUncharged, R_frac_mistake, pT_frac_mistake;
-//    //set tree branches
-//
-//
-//    T->Branch("R_frac_mistake", &R_frac_mistake, "R_frac_mistake");
-//    T->Branch("pT_frac_mistake", &pT_frac_mistake, "pT_frac_mistake");
-//    T->Branch("hasUncharged", &hasUncharged, "hasUncharged");
-//    T->Branch("l11", &l11, "l11");
-//    T->Branch("l105", &l105, "l105");
-//    T->Branch("l115", &l115, "l115");
-//    T->Branch("l12", &l12, "l12");
-//    T->Branch("l13", &l13, "l13");
-//    T->Branch("l20", &l20, "l20");
-//    T->Branch("z_val", &z_val, "z_val");
-//    T->Branch("D_0_pT", &D_0_pT, "D_0_pT");
-//    T->Branch("jet_pT", &Jet_pT, "jet_pT");
-//    T->Branch("eta", &rapidity, "eta");
-//
-//    for (int i = 0; i < numThreads; i++)
-//        alocatedThreads[i] = new std::thread(mainSec, numThreads, std::to_string(seed + i), std::ref(T), std::ref(D_0_pT), std::ref(Jet_pT), std::ref(rapidity),
-//                                             std::ref(z_val), std::ref(requiredNumberOfD_0), std::ref(foundNumberOfD_0),
-//                                             std::ref(l11), std::ref(l105), std::ref(l115), std::ref(l12), std::ref(l13), std::ref(l20),
-//                                             std::ref(hasUncharged), std::ref(R_frac_mistake), std::ref(pT_frac_mistake));
-//
-//    for (int i = 0; i < numThreads; i++) {
-//        alocatedThreads[i]->join();
-//        std::cout << std::endl << "Thread " << i + 1 << " joined" << std::endl;
-//    }
-//
-//    T->Print(); //prints the tree structure
-//    T->Write(); //writes the tree to the file
-//    file->Close();
-//    delete file;
+    unsigned int requiredNumberOfD_0 = 100000;
+    unsigned int foundNumberOfD_0 = 0; //to store number of D_0 particles found
+    unsigned int numThreads = std::thread::hardware_concurrency();
+    int seed = std::time(0) % (900000000 - numThreads);
+    std::thread **alocatedThreads = new std::thread *[numThreads];
+    std::string pathToTheFile = "../results/";
+    std::string filename = "jet tree, cut [D_0 n =" + std::to_string(requiredNumberOfD_0) + ", 1+ GeV].root";
+    TTree *T = new TTree("T", "saves Pt, pseudo rapidity and phi of an D_0 jet"); //create a tree to store the data
+    TFile *file = new TFile( (pathToTheFile + filename).c_str(), "RECREATE"); //create a file to store the tree as an output
+    Float_t D_0_pT = -999, Jet_pT = -999, rapidity = -999, z_val = -999; //variables to store data and used in tree
+    Float_t l11, l105, l115, l12, l13, l20;
+    bool hasUncharged, R_frac_mistake, pT_frac_mistake;
+    //set tree branches
+
+
+    T->Branch("R_frac_mistake", &R_frac_mistake, "R_frac_mistake");
+    T->Branch("pT_frac_mistake", &pT_frac_mistake, "pT_frac_mistake");
+    T->Branch("hasUncharged", &hasUncharged, "hasUncharged");
+    T->Branch("l11", &l11, "l11");
+    T->Branch("l105", &l105, "l105");
+    T->Branch("l115", &l115, "l115");
+    T->Branch("l12", &l12, "l12");
+    T->Branch("l13", &l13, "l13");
+    T->Branch("l20", &l20, "l20");
+    T->Branch("z_val", &z_val, "z_val");
+    T->Branch("D_0_pT", &D_0_pT, "D_0_pT");
+    T->Branch("jet_pT", &Jet_pT, "jet_pT");
+    T->Branch("eta", &rapidity, "eta");
+
+    for (int i = 0; i < numThreads; i++)
+        alocatedThreads[i] = new std::thread(mainSec, numThreads, std::to_string(seed + i), std::ref(T), std::ref(D_0_pT), std::ref(Jet_pT), std::ref(rapidity),
+                                             std::ref(z_val), std::ref(requiredNumberOfD_0), std::ref(foundNumberOfD_0),
+                                             std::ref(l11), std::ref(l105), std::ref(l115), std::ref(l12), std::ref(l13), std::ref(l20),
+                                             std::ref(hasUncharged), std::ref(R_frac_mistake), std::ref(pT_frac_mistake));
+
+    for (int i = 0; i < numThreads; i++) {
+        alocatedThreads[i]->join();
+        std::cout << std::endl << "Thread " << i + 1 << " joined" << std::endl;
+    }
+
+    T->Print(); //prints the tree structure
+    T->Write(); //writes the tree to the file
+    file->Close();
+    delete file;
 
 
     return 0;
