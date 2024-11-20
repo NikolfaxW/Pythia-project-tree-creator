b44d082679d95e624b7280cbb468e349e0371e50
NikolfaxW <denisnik@cvut.cz>
2024-11-20 17:50:21 +0100

    New functions and test of new features, statistics of angularities are done

--- functions.cpp
+++ functions.cpp
@@ -7,11 +7,67 @@
 #include "MyInfo.h"
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdio>
 
 #include "Pythia8/Pythia.h"
 #include "TVector2.h"
 #include "fastjet/PseudoJet.hh"
 #include "fastjet/ClusterSequence.hh"
+
+std::string createBlock( const unsigned int x, const unsigned int y, std::string fill, const unsigned int id, const unsigned int status, const unsigned int evi){
+    std::string result = "<rect x = \"" + std::to_string(x) + "\" y = \"" + std::to_string(y) + "\" width = \"150\" height = \"100\" fill = \"" + fill + "\" stroke = \"black\" stroke-width = \"2\"></rect>\n";
+    result.append("<text x = \"" + std::to_string(x + 75) + "\" y = \"" + std::to_string(y + 60) + "\" font-size = \"16\" text-anchor = \"middle\" fill = \"black\">" + "Id : " + std::to_string(id) + " ; St : " + std::to_string(status) + " ; Ei : " + std::to_string(evi) + "</text>\n");
+    return result;
+}
+
+std::string createArrow(const unsigned int x1, const unsigned int y1, const unsigned int x2, const unsigned int y2){
+    std::string result = "<line x1 = \"" + std::to_string(x1) + "\" y1 = \"" + std::to_string(y1) + "\" x2 = \"" + std::to_string(x2) + "\" y2 = \"" + std::to_string(y2) + "\" stroke = \"black\" stroke-width = \"2\"></line>\n";
+    result.append("<polygon points = \"" + std::to_string(x2) + "," + std::to_string(y2 - 5) + " " + std::to_string(x2) + "," + std::to_string(y2 + 5) + " " + std::to_string(x2 + 10) + "," + std::to_string(y2) + "\" fill = \"black\"></polygon>\n");
+    return result;
+}
+
+void drawBlocksTest() {
+    std::remove("../results/block_diagram.html");
+    // Create an HTML file
+    std::ofstream htmlFile("../results/block_diagram.html");
+
+    if (!htmlFile) {
+        std::cerr << "Error creating file." << std::endl;
+        return;
+    }
+
+    // Write the HTML content
+    htmlFile << R"(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Block Diagram</title>
+           <style>
+                body {
+                    font-family: 'Arial', sans-serif;
+                }
+            </style>
+</head>
+<body>
+    <svg viewBox="0 0 500 200" style="width: 20%; height: 20%;">)";
+//    <!-- Block 1 -->
+    htmlFile << createBlock(50,50, "lightblue", 1, 1, 1);
+    htmlFile << createBlock(300,50, "lightgreen", 2, 1, 1);
+    htmlFile << createArrow(200, 100, 300, 100);
+
+    htmlFile << R"(
+        </svg>
+</body>
+</html>)";
+    htmlFile.close();
+
+    std::cout << "HTML file created: block_diagram.html" << std::endl;
+}
+
 
 
 void showProgressBar(int progress, int total) {
@@ -21,9 +77,9 @@ void showProgressBar(int progress, int total) {
     std::cout.flush();
 }
 
-Float_t delta_R(Float_t eta1, Float_t phi1, Float_t eta2, Float_t phi2) {
-    Float_t delta_eta= eta1 - eta2;
-    Float_t delta_phi = TVector2::Phi_mpi_pi(phi1 - phi2);
+Double_t delta_R(Double_t eta1, Double_t phi1, Double_t eta2, Double_t phi2) {
+    Double_t delta_eta = eta1 - eta2;
+    Double_t delta_phi = TVector2::Phi_mpi_pi(phi1 - phi2);
 //    std::cout << "delta_eta: " << delta_eta << " delta_phi: " << delta_phi << std::endl;
     return TMath::Sqrt(pow(delta_eta, 2) + pow(delta_phi, 2));
 }
@@ -90,9 +146,9 @@ void learnD_0JetsOrigin(const unsigned int requiredNumberOfD_0){
 }
 
 
-void mainSec(const int numThreads, std::string  seed, TTree *&T, Float_t &D_0_pT, Float_t &Jet_Pt, Float_t &rapidity,
-             Float_t &z_val, const unsigned int & requiredNumberOfD_0, unsigned int & numberOfD_0Found,
-             Float_t &l11, Float_t &l105, Float_t &l115, Float_t &l12, Float_t &l13, Float_t &l20){
+void mainSec(const int numThreads, std::string seed, TTree *&T, Float_t &D_0_pT, Float_t &Jet_Pt, Float_t &rapidity,
+             Float_t &z_val, const unsigned int &requiredNumberOfD_0, unsigned int &numberOfD_0Found,
+             Float_t &l11, Float_t &l105, Float_t &l115, Float_t &l12, Float_t &l13, Float_t &l20) {
     Pythia8::Pythia pythia; //create pythia object
 
     {
@@ -115,8 +171,8 @@ void mainSec(const int numThreads, std::string  seed, TTree *&T, Float_t &D_0_pT
     double pTMaxTrig = 5.0;
     double mTemp; //This variable are needed to recount momentum after particle mass resets
     Pythia8::Vec4 pTemp; //This variable are needed to recount momentum after particle mass resets
-
-    Float_t temp_z_val, temp_D_0_pT, temp_l11, temp_l105, temp_l115, temp_l12, temp_l13, temp_l20, R_frac, pT_frac;
+    ;
+    Double_t R_frac, pT_frac, temp_z_val, temp_D_0_pT, temp_l11, temp_l105, temp_l115, temp_l12, temp_l13, temp_l20;
 
     //define jet finding algorithms here:
     jetDefs["#it{k_{t}} jets, #it{R} = " + std::to_string(R)] = fastjet::JetDefinition(
@@ -134,7 +190,7 @@ void mainSec(const int numThreads, std::string  seed, TTree *&T, Float_t &D_0_pT
         int idxD = -1; // to store index of the D_0 particle in the event
         for (int i = pythia.event.size() - 1; i > 0; i--) { //goes through all particles generated in event
             if (pythia.event[i].idAbs() == triggerId &&
-                pythia.event[i].pT() >= pTMinTrig ) { //finds D_0 particle with required pT cut
+                pythia.event[i].pT() >= pTMinTrig) { //finds D_0 particle with required pT cut
                 idxD = i; //saved its index in the event
                 break;
             }
@@ -151,9 +207,11 @@ void mainSec(const int numThreads, std::string  seed, TTree *&T, Float_t &D_0_pT
                     mTemp = 0;
                 else
                     mTemp = 0.13957;
-                fastjet::PseudoJet particleTemp = fastjet::PseudoJet(p.px(), p.py(), p.pz(), sqrt(pTemp.pAbs2() + mTemp *
-                                                                                                                  mTemp)); //recounts 4 momentum and energy after mass reset
-                particleTemp.set_user_info(new MyInfo(p.idAbs(), i, event[i].isCharged())); //adds additional info to the particle
+                fastjet::PseudoJet particleTemp = fastjet::PseudoJet(p.px(), p.py(), p.pz(),
+                                                                     sqrt(pTemp.pAbs2() + mTemp *
+                                                                                          mTemp)); //recounts 4 momentum and energy after mass reset
+                particleTemp.set_user_info(
+                        new MyInfo(p.idAbs(), i, event[i].isCharged())); //adds additional info to the particle
                 fjInputs.push_back(particleTemp); //saves the particle to the vector to be clustered
 
             }
@@ -184,7 +242,7 @@ void mainSec(const int numThreads, std::string  seed, TTree *&T, Float_t &D_0_pT
                         break;
                     }
                 }
-                if(not temp_Has_D_0) continue; // if there is not d_0 particle in the jet, skip it;
+                if (not temp_Has_D_0) continue; // if there is not d_0 particle in the jet, skip it;
                 temp_l11 = 0;
                 temp_l105 = 0;
                 temp_l115 = 0;
@@ -194,15 +252,16 @@ void mainSec(const int numThreads, std::string  seed, TTree *&T, Float_t &D_0_pT
 
 
                 for (const auto &c: jet.constituents()) { //loop through all jet constituents to calculate l11, l105, l115, l12, l13, l20
-                    if(c.user_info<MyInfo>().pdg_id() != triggerId && !c.user_info<MyInfo>().isCharged()) continue; //skip not D_0 and neutral particles
+                    if (c.user_info<MyInfo>().pdg_id() != triggerId && !c.user_info<MyInfo>().isCharged())
+                        continue; //skip not D_0 and neutral particles
                     pT_frac = c.pt() / jet.pt();
                     R_frac = delta_R(jet.eta(), jet.phi(), c.eta(), c.phi()) / R;
-                    temp_l11 += pT_frac*R_frac;
+                    temp_l11 += pT_frac * R_frac;
                     temp_l105 += pT_frac * pow(R_frac, 0.5);
                     temp_l115 = pT_frac * pow(R_frac, 1.5);
                     temp_l12 += pT_frac * pow(R_frac, 2);
                     temp_l13 += pT_frac * pow(R_frac, 3);
-                    temp_l20 += pow(pT_frac,2);
+                    temp_l20 += pow(pT_frac, 2);
                 }
 
 
@@ -221,7 +280,7 @@ void mainSec(const int numThreads, std::string  seed, TTree *&T, Float_t &D_0_pT
                     rapidity = jet.rapidity();
                     T->Fill();  // Fill the data vector safely
                     ++numberOfD_0Found;
-                    if(numberOfD_0Found % 10 == 0) showProgressBar(numberOfD_0Found, requiredNumberOfD_0);
+                    if (numberOfD_0Found % 10 == 0) showProgressBar(numberOfD_0Found, requiredNumberOfD_0);
                 }
             }
 
--- functions.h
+++ functions.h
@@ -8,9 +8,10 @@
 #include "Rtypes.h"
 #include "TTree.h"
 
+void drawBlocksTest();
 
 void showProgressBar(int progress, int total);
-Float_t delta_R(Float_t eta1, Float_t phi1, Float_t eta2, Float_t phi2);
+Double_t delta_R(Double_t eta1, Double_t phi1, Double_t eta2, Double_t phi2);
 void learnD_0JetsOrigin(const unsigned int requiredNumberOfD_0);
 void mainSec(const int numThreads, std::string  seed, TTree *&T, Float_t &D_0_pT, Float_t &Jet_Pt, Float_t &rapidity,
              Float_t &z_val, const unsigned int & requiredNumberOfD_0, unsigned int & numberOfD_0Found,
